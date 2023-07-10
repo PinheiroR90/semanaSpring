b324627395616e8f63d762b4de2a11281c3e867f
RobsonP <pinheirorobson7@gmail.com>
2023-07-10 18:16:05 -0300

    First commit

---
+++ src/main/java/com/example/project/pinheiro/PinheiroApplication.java
@@ -0,0 +1,14 @@
+package com.example.project.pinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinheiroApplication.class, args);
+	     System.out.print("Aplicação rodando");
+	}
+
+}
---
+++ src/test/java/com/example/project/pinheiro/PinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.pinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
